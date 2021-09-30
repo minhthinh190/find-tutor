e2d913a41a03af362086a55eaeda57f0e23be966
minhthinh190 <dadzaikitano@gmail.com>
2021-09-30 23:27:27 +0700

    fix code style

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 export default function ({ store, redirect }) {
   const isAuthenticated = store.state.auth.userCredential ? true : false
+
   if (!isAuthenticated) {
     console.log('hello:', isAuthenticated)
     redirect({ name: 'signin' })
