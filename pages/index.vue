16a2bfd7c683bf359901840d8a3ff00acced27d4
thinhtm <thinhtm@miichisoft.net>
2021-10-06 23:41:09 +0700

    display user data

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     <AppBar />
+    <p>Welcome! {{ user.email }}</p>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import AppBar from '~/components/AppBar'
 
 export default {
   middleware: 'auth',
   components: {
     AppBar
+  },
+  computed: {
+    ...mapState({
+      user: state => state.auth.user
+    })
   }
 }
 </script>
