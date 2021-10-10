bb3f477fbf6fba46fc32cee1d8856fbcbb3790b3
minhthinh190 <dadzaikitano@gmail.com>
2021-10-10 16:08:55 +0700

    Remove unused user info

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,16 @@
 <template>
   <div>
     <AppBar />
-    <p>Welcome! {{ user.email }}</p>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 import AppBar from '~/components/AppBar'
 
 export default {
   middleware: 'auth',
   components: {
     AppBar
-  },
-  computed: {
-    ...mapState({
-      user: state => state.auth.user
-    })
   }
 }
 </script>
