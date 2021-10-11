983ee479eef0e3136d65f74126ca1cdd3b5236d7
minhthinh190 <dadzaikitano@gmail.com>
2021-10-12 00:27:19 +0700

    Add layout notification

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,26 @@
     <v-main>
       <Nuxt />
     </v-main>
+
+    <v-snackbar
+      v-model="isNotiShowed"
+      :color="notiColor"
+      text
+    >
+      {{ notiMessage }}
+    </v-snackbar>
   </v-app>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState({
+    notiMessage: state => state.notification.message,
+    notiColor: state => state.notification.color,
+    isNotiShowed: state => state.notification.isShowed
+  })
+}
+</script>
+
