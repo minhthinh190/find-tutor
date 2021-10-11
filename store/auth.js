9df53ad621b55bdb553772dd38c550bfedeb5eef
minhthinh190 <dadzaikitano@gmail.com>
2021-10-12 00:26:32 +0700

    Fix code style

--- store/auth.js
+++ store/auth.js
@@ -18,48 +18,27 @@ export const mutations = {
 
 export const actions = {
   signIn (context, { email, password }) {
-    userService.signUserIn(email, password)
+    return userService.signUserIn(email, password)
       .then(userCredential => {
         const { uid, email } = userCredential.user
         context.commit('setUser', { uid, email })
         return uid
-      })
-      .then(uid => {
-        localStorage.setItem('uid', uid)
-        $nuxt.$router.push({ name: 'index' })
-      })
-      .catch(err => {
-        console.log('Sign in error:', err)
       })
   },
 
   signUp (context, { email, password }) {
-    userService.signUserUp(email, password)
+    return userService.signUserUp(email, password)
       .then(userCredential => {
         const { uid, email } = userCredential.user
         context.commit('setUser', { uid, email })
         return uid
       })
-      .then(uid => {
-        localStorage.setItem('uid', uid)
-        $nuxt.$router.push({ name: 'index' })
-      })
-      .catch(err => {
-        console.log('Sign up error:', err)
-      })
   },
 
   signOut (context) {
-    userService.signUserOut()
+    return userService.signUserOut()
       .then(() => {
         context.commit('setUser', null)
-      })
-      .then(() => {
-        localStorage.removeItem('uid')
-        $nuxt.$router.push({ name: 'signin' })
-      })
-      .catch(err => {
-        console.log('Sign out error:', err)
       })
   }
 }
