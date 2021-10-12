f415121b83fed4dc9f4ddc748a286bcb53ea29af
minhthinh190 <dadzaikitano@gmail.com>
2021-10-13 00:38:43 +0700

    Update notification store

--- store/notification.js
+++ store/notification.js
@@ -1,27 +1,17 @@
 export const state = () => ({
   message: null,
-  color: null,
-  isShowed: false
+  color: null
 })
 
 export const mutations = {
   setNotification (state, data) {
     state.message = data.message
     state.color = data.color
-    state.isShowed = true
-  },
-
-  removeNotification (state) {
-    state.isShowed = false
   }
 }
 
 export const actions = {
   showNotification (context, data) {
     context.commit('setNotification', data)
-  },
-
-  hideNotification (context) {
-    context.commit('removeNotification')
   }
 }
