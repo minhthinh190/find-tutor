69eb5b663995ced13682937610f30bef28d87ef9
minhthinh190 <dadzaikitano@gmail.com>
2021-10-13 00:42:17 +0700

    Add displaying noti mechanism

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,6 +10,17 @@
       text
     >
       {{ notiMessage }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          v-bind="attrs"
+          icon
+          :color="notiColor"
+          @click="isNotiShowed = false"
+        >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </template>
     </v-snackbar>
   </v-app>
 </template>
@@ -18,11 +29,22 @@
 import { mapState } from 'vuex'
 
 export default {
+  data () {
+    return {
+      isNotiShowed: false
+    }
+  },
   computed: mapState({
     notiMessage: state => state.notification.message,
-    notiColor: state => state.notification.color,
-    isNotiShowed: state => state.notification.isShowed
-  })
+    notiColor: state => state.notification.color
+  }),
+  created () {
+    this.$store.subscribe((mutations, state) => {
+      if (mutations.type === 'notification/setNotification') {
+        this.isNotiShowed = false
+        this.isNotiShowed = true
+      }
+    })
+  }
 }
 </script>
-
