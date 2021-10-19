11a9eb23c07851940c9962ca281a994db7ee233f
minhthinh190 <dadzaikitano@gmail.com>
2021-10-19 23:54:56 +0700

    Remove bad store actions

--- store/booking.js
+++ store/booking.js
@@ -1,5 +1,3 @@
-import { dbService } from "~/firebase/database"
-
 export const state = () => ({
   list: null
 })
@@ -10,42 +8,4 @@ export const mutations = {
   }
 }
 
-export const actions = {
-  sendBookingRequest (context, request) {
-    const destination = {
-      collection: 'user',
-      docID: 'johndoe2@email.com',
-      subCollection: 'bookings',
-      subDocID: '9'
-    }
-    return dbService.addDataToSubDoc(
-      destination.collection,
-      destination.docID,
-      destination.subCollection,
-      destination.subDocID,
-      request
-    )
-  },
-
-  async updateBookingListLength () {
-    let bookingCount = null
-
-    const destination = {
-      collection: 'user',
-      docID: 'johndoe2@email.com'
-    }
-    await dbService.getDataInDoc(destination.collection, destination.docID)
-      .then((docSnap) => {
-        bookingCount = docSnap.data().bookingCount
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-
-    dbService.updateDataInDoc(
-      destination.collection,
-      destination.docID,
-      { bookingCount: bookingCount + 1 }
-    )
-  }
-}
+export const actions = {}
