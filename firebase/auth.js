5fac0ec472e3c5d01b1e430f45c2ac5fa84dcc7f
minhthinh190 <dadzaikitano@gmail.com>
2021-10-21 23:23:13 +0700

    Rename firebase authentication file

---
+++ firebase/auth.js
@@ -0,0 +1,27 @@
+import app from '~/firebase/config.js'
+import { getAuth } from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from 'firebase/auth'
+
+const auth = getAuth(app)
+
+const signUserIn = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password)
+}
+
+const signUserUp = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password)
+}
+
+const signUserOut = () => {
+  return signOut(auth)
+}
+
+export const authService = {
+  signUserIn,
+  signUserUp,
+  signUserOut
+}
