96893df8e19f6a74185518898f5862e269acb542
minhthinh190 <dadzaikitano@gmail.com>
2021-10-21 23:26:03 +0700

    Add getters

--- store/booking.js
+++ store/booking.js
@@ -2,6 +2,12 @@ export const state = () => ({
   list: null
 })
 
+export const getters = {
+  numberOfBookings: (state) => {
+    return state.list.length
+  } 
+}
+
 export const mutations = {
   setList (state, list) {
     state.list = list
