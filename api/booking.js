80ceaf3ade126787e1c17a6a198c39c74720b51a
minhthinh190 <dadzaikitano@gmail.com>
2021-10-24 15:01:57 +0700

    Fix booking api data flow

--- api/booking.js
+++ api/booking.js
@@ -1,66 +1,83 @@
-import { dbService } from "~/firebase/database"
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  updateDoc
+} from 'firebase/firestore'
 
+const db = getFirestore()
 const _rootCollection = 'user'
 const _collection = 'bookings'
 
 const createBookingCounter = async (userDoc) => {
-  await dbService.addDataToDoc(_rootCollection, userDoc, {
+  const docRef = doc(db, _rootCollection, userDoc)
+  await setDoc(docRef, {
     bookingCount: 0,
     bookingIdCount: 0
   })
 }
 
 const createNewBooking = async (userDoc, bookingData) => {
-  let currentBookingId = await getBookingIdCount(userDoc)
-  currentBookingId++
+  const currentBookingId = await getBookingIdCount(userDoc)
+  const newBookingId = currentBookingId + 1
 
-  return dbService.addDataToSubDoc(
-    _rootCollection,
-    userDoc,
-    _collection,
-    currentBookingId.toString(),
-    bookingData
-  )
+  const docRef = doc(db, _rootCollection, userDoc, _collection, newBookingId.toString())
+
+  return setDoc(docRef, { id: newBookingId, ...bookingData })
     .then(async () => {
       await updateBookingIdCount(userDoc)
       await updateNumberOfBookings(userDoc)
     })
-},
+}
 
-///
-const getBookingList = async (userDoc) => {}
-///
+const getBookingList = async (userDoc) => {
+  const querySnapshot = await getDocs(
+    collection(db, _rootCollection, userDoc, _collection)
+  )
+  const bookingList = querySnapshot.map((doc) => doc.data())
+  return bookingList
+}
 
 const getBookingIdCount = async (userDoc) => {
-  const res = await dbService.getDataInDoc(_rootCollection, userDoc)
+  const docRef = doc(db, _rootCollection, userDoc)
+  const res = await getDoc(docRef)
   const bookingIdCount = res.data().bookingIdCount
+
   return bookingIdCount
 }
 
 const getNumberOfBookings = async (userDoc) => {
-  const res = await dbService.getDataInDoc(_rootCollection, userDoc)
+  const docRef = doc(db, _rootCollection, userDoc)
+  const res = await getDoc(docRef)
   const bookingCount = res.data().bookingCount
+
   return bookingCount
 }
 
 const updateNumberOfBookings = async (userDoc) => {
   const numberOfBookings = await getNumberOfBookings(userDoc)
+  const docRef = doc(db, _rootCollection, userDoc)
 
-  dbService.updateDataInDoc(_rootCollection, userDoc, {
+  updateDoc(docRef, {
     bookingCount: numberOfBookings + 1
   })
 }
 
 const updateBookingIdCount = async (userDoc) => {
   const bookingIdCount = await getBookingIdCount(userDoc)
+  const docRef = doc(db, _rootCollection, userDoc)
 
-  dbService.updateDataInDoc(_rootCollection, userDoc, {
+  updateDoc(docRef, {
     bookingIdCount: bookingIdCount + 1
   })
 }
 
 export const bookingAPI = {
   createBookingCounter,
+  getBookingList,
   createNewBooking,
   getNumberOfBookings
 }
