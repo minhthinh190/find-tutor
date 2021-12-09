0a6a4d83f7019fc4ee00e80c988930f770f5b00e
minhthinh190 <dadzaikitano@gmail.com>
2021-12-10 00:27:23 +0700

    Paginate tutor list

--- store/tutor.js
+++ store/tutor.js
@@ -1,11 +1,18 @@
 import { tutorAPI } from '~/api/tutor'
 
 export const state = () => ({
+  perPage: 5,
+  tutors: [],
+  paginatedTutors: [],
   applyingTutors: [],
   tutorProfile: {}
 })
 
-export const getters = {}
+export const getters = {
+  paginatedTutors: (state) => {
+    return state.paginatedTutors
+  }
+}
 
 export const mutations = {
   setTutorProfile (state, tutorProfile) {
@@ -14,17 +21,43 @@ export const mutations = {
 
   setApplyingTutors (state, applyingTutors) {
     state.applyingTutors = applyingTutors
+  },
+
+  setPaginatedTutors (state, paginatedTutors) {
+    state.paginatedTutors = paginatedTutors
+  },
+
+  setTutors (state, tutors) {
+    state.tutors = tutors
   }
 }
 
 export const actions = {
+  paginateTutorList ({ state, commit }) {
+    const paginatedTutorList = state.tutors.reduce((resultArr, item, index) => {
+      const pageIndex = Math.floor(index / state.perPage)
+      if (!resultArr[pageIndex]) {
+        resultArr[pageIndex] = []
+      }
+      resultArr[pageIndex].push(item)
+
+      return resultArr
+    }, [])
+    commit('setPaginatedTutors', paginatedTutorList)
+  },
+
+  async getTutors ({ commit }) {
+    const tutors = await tutorAPI.getAllTutors()
+    commit('setTutors', tutors)
+  },
+
   async getApplyingTutors ({ commit }, applyingTutorEmails) {
     const applyingTutors = await tutorAPI.getApplyingTutors(applyingTutorEmails)
     commit('setApplyingTutors', applyingTutors)
   },
 
   async getTutorProfile ({ commit }, tutorEmail) {
-    const tutorProfile = await tutorAPI.getTutorProfile(tutorEmail)
+    const tutorProfile = await tutorAPI.getTutor(tutorEmail)
     commit('setTutorProfile', tutorProfile)
   }
 }
